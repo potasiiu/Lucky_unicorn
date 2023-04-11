6fbb0cdf2b421451553b809387d707a2fd1bdde1
甄耀东 <zhent@middleton.school.nz>
2023-04-11 13:30:03 +1200

    update

---
+++ LU-V3-Yes No function.py
@@ -0,0 +1,34 @@
+"""
+Lucky Unicorn Pre-Assessment - V3
+Created By: Terry Zhen
+"""
+
+
+# Yes no function
+def yes_no(question_text):
+    while True:
+
+        # Ask the user if they have played before
+        answer = input(question_text).lower()
+
+        # Show the user what they inputted
+
+
+        # If the user picked yes then start the game
+        if answer == "yes" or answer == "y":
+            answer = "Yes"
+            return answer
+
+        # If the user picked no show instructions
+        elif answer == "no" or answer == "n":
+            answer = "No"
+            return answer
+
+        # If the user enters anything else display error and ask again
+        else:
+         print("<error> Please Enter y/n")
+
+
+# Main Routine
+question = yes_no("Have you played before? ")
+print(f"You have entered '{question}'")
--- LU-V3.py
+++ LU-V3.py
@@ -3,22 +3,23 @@ Lucky Unicorn Pre-Assessment - V3
 Created By: Terry Zhen
 """
 # While loop to increase testing efficiency
+show_instructions = ""
+while show_instructions != "x":
+    # Ask the user if they have played before
+    show_instructions = input("Have you played before? ").lower()
 
-# Ask the user if they have played before
-show_instructions = input("Have you played before? ").lower()
+    # Show the user what they inputted
+    print(f"You have entered '{show_instructions}'")
 
-# Show the user what they inputted
-print(f"You have entered '{show_instructions}'")
+    # If the user picked yes then start the game
+    if show_instructions == "yes" or show_instructions == "y":
+        print("Game Start")
 
-# If the user picked yes then start the game
-if show_instructions == "yes" or show_instructions == "y":
-    print("Game Start")
+    # If the user picked no show instructions
+    elif show_instructions == "no" or show_instructions == "n":
+        print("Display Instructions")
 
-# If the user picked no show instructions
-elif show_instructions == "no" or show_instructions == "n":
-    print("Display Instructions")
-
-# If the user enters anything else display error and ask again
-else:
-    print("<error> Please Enter y/n")
+    # If the user enters anything else display error and ask again
+    else:
+        print("<error> Please Enter y/n")
 
